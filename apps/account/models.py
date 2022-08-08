e8c3e4509cd3c46bbadadd75a5d27d38e85be105
Earthtosarah <akinjisolasarah02@gmail.com>
2022-08-08 07:19:38 +0100

    user profile model(contd)

--- apps/account/models.py
+++ apps/account/models.py
@@ -35,7 +35,15 @@ class UserProfileManager(BaseUserManager):
         
         return user
 
-    def create_superuser(self, email, name, password)
+    def create_superuser(self, email, first_name, last_name, password):
+        """create and saves a new user with the given details"""
+        
+        user=self.create_user(email, first_name, last_name, password)
+        user.is_superuser=True
+        user.is_staff=True
+        
+        user.save(using=self._db)
+        return user
 #expanded model
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """represents a user profile in our database"""
