f03c18a97d618a62080be2fd89ebbb25473d3a46
Habeeb Shopeju <shopejuh@gmail.com>
2022-08-09 21:48:43 +0100

    Integrated the favicon app into the django project

--- team66/wsgi.py
+++ team66/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team66.settings')
 
