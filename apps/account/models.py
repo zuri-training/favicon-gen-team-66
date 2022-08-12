87d27fd2f4206f8121f55f00bc0ea2de3f7fc8a1
Pauline.B <paulinebanye@gmail.com>
2022-08-12 19:23:50 +0100

    updated user profile imgs

--- apps/account/models.py
+++ apps/account/models.py
@@ -55,6 +55,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     name=models.CharField(max_length = 100, null = True)
     title = models.CharField(max_length = 50, blank = True)
     profile_pic = models.ImageField(null = True, blank = True)
+    # profile_pic = models.URLField(max_length = 500,null = True, blank = True)
     email = models.EmailField(max_length = 254, unique=True, editable=True, null=True)
     date_created = models.DateTimeField(auto_now_add = True, null = True)
     date_updated = models.DateTimeField(auto_now = True)
--- team66/settings.py
+++ team66/settings.py
@@ -147,13 +147,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR/'frontend/templates/account/images/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/'frontend/static/'
     ]
-MEDIA_ROOT = [
-    BASE_DIR/'frontend/templates/account/images/'
-    ]
+
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
