83ed2bc37a9e0f896622b4d6d6d8cf3685f54992
Pauline.B <paulinebanye@gmail.com>
2022-08-12 18:19:21 +0100

    updated user profile

--- team66/settings.py
+++ team66/settings.py
@@ -151,9 +151,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/'frontend/static/'
     ]
-# MEDIA_ROOT = [
-#     BASE_DIR/'frontend/templates/account/images/'
-#     ]
+MEDIA_ROOT = [
+    BASE_DIR/'frontend/templates/account/images/'
+    ]
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
